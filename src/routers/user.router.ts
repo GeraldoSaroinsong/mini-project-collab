4aa9a277ee308058a7f0e0f00a7e3590084660d1
Odlareg <78185462+GeraldoSaroinsong@users.noreply.github.com>
2024-12-18 10:40:21 +0700

    Prepare deploy (#13)
    * prepare-deployment-in-vercel
    
    * tsconfig-mod
    
    * tsconfig-update-and-build
    
    * user-router-get-fix

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -8,42 +8,38 @@ import { verifyToken } from "../middleware/verifyToken";
 // import { regisValidation } from "../middleware/validator";
 
 export class UserRouter {
-    private route: Router;
-    // ? DEFINE PROPS HERE. CONTOH:
-    private userController: UserController;
+  private route: Router;
+  // ? DEFINE PROPS HERE. CONTOH:
+  private userController: UserController;
 
-    constructor() {
-        // ? INSTANTIATE ROUTER
-        this.route = Router();
-        // ? INSTANTIATE CONTROLLER HERE. CONTOH:
-        this.userController = new UserController();
+  constructor() {
+    // ? INSTANTIATE ROUTER
+    this.route = Router();
+    // ? INSTANTIATE CONTROLLER HERE. CONTOH:
+    this.userController = new UserController();
 
-        this.initializeRoutes();
-    }
+    this.initializeRoutes();
+  }
 
-    private initializeRoutes(): void {
-        // ? DEFINE ENDPOINTS HERE. CONTOH
-        this.route.post(
-            "/register",
-            regisValidation,
-            this.userController.registerUser
-        );
-        this.route.post(
-            "/login",
-            loginValidation,
-            this.userController.loginUser
-        );
-        this.route.patch("/update/:id", this.userController.updateUser);
-        this.route.delete("/delete/:id", this.userController.deleteUser);
-        this.route.post(
-            "/keep-login",
-            verifyToken,
-            this.userController.keepLoginUser
-        );
-        this.route.get("/user/:id", this.userController.registerUser);
-    }
+  private initializeRoutes(): void {
+    // ? DEFINE ENDPOINTS HERE. CONTOH
+    this.route.post(
+      "/register",
+      regisValidation,
+      this.userController.registerUser
+    );
+    this.route.post("/login", loginValidation, this.userController.loginUser);
+    this.route.patch("/update/:id", this.userController.updateUser);
+    this.route.delete("/delete/:id", this.userController.deleteUser);
+    this.route.post(
+      "/keep-login",
+      verifyToken,
+      this.userController.keepLoginUser
+    );
+    this.route.get("/user/:id", this.userController.uniqueUser);
+  }
 
-    public getRouter(): Router {
-        return this.route;
-    }
+  public getRouter(): Router {
+    return this.route;
+  }
 }
