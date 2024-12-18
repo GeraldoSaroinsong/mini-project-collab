ec29f84d0b70bec0649314abbc0b0c86a96b546e
Odlareg <78185462+GeraldoSaroinsong@users.noreply.github.com>
2024-12-18 10:43:05 +0700

    Prepare deploy (#14)
    * prepare-deployment-in-vercel
    
    * tsconfig-mod
    
    * tsconfig-update-and-build
    
    * user-router-get-fix
    
    * user-router-get-fix-2

--- dist/routers/user.router.js
+++ dist/routers/user.router.js
@@ -23,7 +23,8 @@ class UserRouter {
         this.route.patch("/update/:id", this.userController.updateUser);
         this.route.delete("/delete/:id", this.userController.deleteUser);
         this.route.post("/keep-login", verifyToken_1.verifyToken, this.userController.keepLoginUser);
-        this.route.get("/user/:id", this.userController.registerUser);
+
+        this.route.get("/user/:id", this.userController.uniqueUser);
     }
     getRouter() {
         return this.route;
