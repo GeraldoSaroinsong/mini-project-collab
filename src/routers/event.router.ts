6c001be7a447334d0e9b78e3e19621df5d823163
Odlareg <78185462+GeraldoSaroinsong@users.noreply.github.com>
2025-01-07 10:27:38 +0700

    event-get-by-title (#21)

--- src/routers/event.router.ts
+++ src/routers/event.router.ts
@@ -3,32 +3,33 @@ import { EventController } from "../controllers/event.controller";
 import { verifyTokenOrganizer } from "../middleware/verifyToken";
 
 export class EventRouter {
-    private route: Router;
-    private eventController = new EventController();
+  private route: Router;
+  private eventController = new EventController();
 
-    constructor() {
-        this.route = Router();
-        this.eventController = new EventController();
-        this.initializeRoutes();
-    }
+  constructor() {
+    this.route = Router();
+    this.eventController = new EventController();
+    this.initializeRoutes();
+  }
 
-    private initializeRoutes(): void {
-        this.route.post(
-            "/create",
-            verifyTokenOrganizer,
-            this.eventController.createEvent
-        );
-        this.route.patch(
-            "/update/:id",
-            verifyTokenOrganizer,
-            this.eventController.updateEvent
-        );
-        this.route.get("/", this.eventController.getEvent);
-        this.route.get("/detail/:title", this.eventController.getEventById);
-        this.route.delete("/delete/:title", this.eventController.deleteEvent);
-    }
+  private initializeRoutes(): void {
+    this.route.post(
+      "/create",
+      verifyTokenOrganizer,
+      this.eventController.createEvent
+    );
+    this.route.patch(
+      "/update/:id",
+      verifyTokenOrganizer,
+      this.eventController.updateEvent
+    );
+    this.route.get("/", this.eventController.getEvent);
+    this.route.get("/all", this.eventController.getEventMany);
+    this.route.get("/detail/:title", this.eventController.getEventByTitle);
+    this.route.delete("/delete/:title", this.eventController.deleteEvent);
+  }
 
-    public getRouter(): Router {
-        return this.route;
-    }
+  public getRouter(): Router {
+    return this.route;
+  }
 }
